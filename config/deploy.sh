9effbe11b6e670506e709d6ca2da4df731e114b0
Jimmy Luo <jimmy.zw.luo@gmailcome>
2019-09-13 14:40:17 -0700

    Delete deploy.sh

--- config/deploy.sh
+++
@@ -1,6 +0,0 @@
-IMAGE_NAME:=config
-IMAGE_VERSION:=v1
-docker build -t gcr.io/$(PROJECT_ID)/$(IMAGE_NAME):$(IMAGE_VERSION) .
-docker push gcr.io/$(PROJECT_ID)/$(IMAGE_NAME):$(IMAGE_VERSION)
-kubectl apply -f k8s.yml
-kubectl patch deployment $(IMAGE_NAME) -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
